bcf8c7f3e933f4621f64bedd2723bb2aba1b0787
Lautaro <lautarovanh@gmail.com>
2026-06-06 17:33:40 -0300

    Se añadio el metodo handler para el manejo de excepciones

--- src/main/java/com/lautaro/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/lautaro/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,5 +1,9 @@
 package com.lautaro.springboot.error.springboot_error.controllers;
 
+import java.util.Date;
+
+import com.lautaro.springboot.error.springboot_error.models.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,9 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice   
 public class HandlerExceptionController {
 
-    @ExceptionHandler(ArithmeticException.class)
-    public ResponseEntity<?> divisionByZero(Exception ex) {
+    @ExceptionHandler({ArithmeticException.class})
+    public ResponseEntity<Error> divisionByZero(Exception ex) {
         Error error = new Error();
-        return ResponseEntity.internalServerError().body("Error 500");
+        error.setDate(new Date());
+        error.setError("Division by zero");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        //return ResponseEntity.internalServerError().body(error);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
