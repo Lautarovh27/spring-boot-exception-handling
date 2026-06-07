ce049e922e5495c605682f07b407078847c700d5
Lautaro <lautarovanh@gmail.com>
2026-06-07 16:01:51 -0300

    Se personalizo el error 404

--- src/main/java/com/lautaro/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/lautaro/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice   
 public class HandlerExceptionController {
@@ -21,5 +22,17 @@ public class HandlerExceptionController {
 
         //return ResponseEntity.internalServerError().body(error);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFound(NoHandlerFoundException ex) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Not found");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(404).body(error);
+        
     }
 }
